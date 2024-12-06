6abdbbe8c999d42208338d9596554e02a2fabc2c
pmvalle <martinezvp.ext@madrid.es>
2024-12-06 19:05:51 +0100

    Refactor

---
+++ src/test/java/org/pmv/myspring/controller/BaseControllerTest.java
@@ -0,0 +1,41 @@
+package org.pmv.myspring.controller;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
+public abstract class BaseControllerTest {
+
+    protected static final String URL_RESTAURANTES_BUSCAR_TODOS = "/api/restaurantes";
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected <T> List<T> httpResponseFrom(String url, TypeReference<List<T>> typeReference) throws Exception {
+        String jsonResponse = mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        return objectMapper.readValue(jsonResponse, typeReference);
+    }
+
+}
